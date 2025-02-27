f34fd1fd6a81820f3a905ee11d908df64929947c
AkAnikMistry <anikmistry8963@gmail.com>
2025-02-27 18:20:08 +0600

    connect mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,28 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+
+
 const app = express()
 
+const connect = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB);
+        console.log("connected mongodb")
+      } catch (error) {
+        throw error;
+      }
+
+}
+
+mongoose.connection.on("disconnected", ()=>{
+  console.log("mongoDB disconnectd")
+})
+
 app.listen(8000,()=>{
+    connect()
     console.log("server is connected")
 })
