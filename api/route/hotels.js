289d14f4460cf51384a847b3813d05e32c1205ce
AkAnikMistry <anikmistry8963@gmail.com>
2025-03-01 02:50:36 +0600

    complete hotel route

--- api/route/hotels.js
+++ api/route/hotels.js
@@ -20,7 +20,9 @@ router.post("/", async (req, res)=>{
 //UPDATE
 router.put("/:id", async(req,res)=>{
     try {
-        const updateHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new : true})
+        const updateHotel = await Hotel.findByIdAndUpdate(req.params.id,
+             {$set: req.body},
+              {new : true})
         console.log(updateHotel)
         res.status(200).json(updateHotel)
     } catch (error) {
@@ -28,6 +30,30 @@ router.put("/:id", async(req,res)=>{
     }
 })
 //DELETE
+router.delete("/:id", async(req,res)=>{
+    try {
+        const updateHotel = await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).json("Hotel has been deleted")
+    } catch (error) {
+        res.status(500).json(err)
+    }
+})
 //GET
+router.get("/:id", async(req,res)=>{
+    try {
+        const hotel = await Hotel.findById(req.params.id)
+        res.status(200).json(hotel)
+    } catch (error) {
+        res.status(500).json(err)
+    }
+})
 //GET ALL
+router.get("/", async(req,res)=>{
+    try {
+        const hotels = await Hotel.find()
+        res.status(200).json(hotels)
+    } catch (error) {
+        res.status(500).json(err)
+    }
+})
 export default router
