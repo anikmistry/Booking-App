2ecb541ad2d260a29212230a063e5ee1264630d5
AkAnikMistry <anikmistry8963@gmail.com>
2025-03-06 01:16:33 +0600

    register and login

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,10 +1,11 @@
 import User from "../models/user_model.js"
 import bcrypt from "bcryptjs"
+import { createError } from "../utils/error.js";
 
 export const register = async (req,res,next)=>{
     try {
-        const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(req.body.password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt);
         const user = new User({
             username: req.body.username,
             email: req.body.email,
@@ -15,4 +16,24 @@ export const register = async (req,res,next)=>{
     } catch (err) {
         next(err)
     }
+}
+
+//login
+
+export const login = async(req,res,next)=>{
+    try {
+        const user =await User.findOne({username:req.body.username})
+
+        if(!user) return next(createError(404,"user is not found"));
+
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if(!isPasswordCorrect) return next(createError(400,"wrong password"));
+        
+        
+        const{ password , isAdmin, ...otherDetails} = user._doc
+        res.status(200).json({...otherDetails})
+    } catch (err) {
+        next(err)
+    }
+
 }
